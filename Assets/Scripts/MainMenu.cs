4bc879258aae156d238e8290c5fbfd34afbb9362
danyachuu <zivkovicdanica522@gmail.com>
2026-03-29 19:24:10 +0200

    Fixed video bugs and added background

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
+using System.IO;
 
 public class MainMenu : MonoBehaviour
 {
+    private static bool hasPlayedIntro = false;
 
     [Header("Intro Settings")]
     public GameObject introObject;
     public VideoPlayer introVideo;
-    public GameObject menuButtons; 
+    public GameObject menuButtons;
 
-    private bool isIntroPlaying = true;
+
+    private bool isIntroPlaying = false;
 
     public GameObject controlsPanel;
 
@@ -22,17 +25,51 @@ public class MainMenu : MonoBehaviour
 
     void Start()
     {
+        if (hasPlayedIntro)
+        {
+            SkipToMenu();
+            return;
+        }
+
+        SetupAndPlayIntro();
+    }
+
+    void SetupAndPlayIntro()
+    {
+        isIntroPlaying = true;
+        hasPlayedIntro = true;
+
         if (introObject) introObject.SetActive(true);
         if (menuButtons) menuButtons.SetActive(false);
 
         if (introVideo != null)
         {
             introVideo.enabled = true;
+
+            string videoName = "Intro Animation.mp4";
+            string videoPath = Path.Combine(Application.streamingAssetsPath, videoName);
+
+            introVideo.source = VideoSource.Url;
+            introVideo.url = videoPath;
+
             introVideo.Play();
             introVideo.loopPointReached += (vp) => EndIntro();
         }
 
         StartCoroutine(AutoEndIntro());
+    }
+
+    void SkipToMenu()
+    {
+        isIntroPlaying = false;
+        if (introVideo != null) introVideo.enabled = false;
+        if (introObject) introObject.SetActive(false);
+        if (menuButtons) menuButtons.SetActive(true);
+
+        if (AudioManager.instance != null)
+        {
+            AudioManager.instance.StartBackgroundMusic();
+        }
     }
 
     void Update()
